c0e4a8b3180c0827b4e3ec48da9a487124a597b2
Lily Whitford <lilywhitford2@gmail.com>
2024-02-23 05:35:15 +0000

    refactor code to take in callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,23 @@
 const request = require("request");
 
-let commandLine = process.argv.slice(2);
-
 // define function that fetches description of a specific cat from cat API
-const fetchBreedDescription = function(breedName) {
+const fetchBreedDescription = function(breedName, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, (error, response, body) => { // make an http GET request
     if (error) { // check if there was an error with the request
-      console.log("Error with GET request.", error); // print out error details
+      callback(error, null); // print out error details
     } else {
       let data = JSON.parse(body); // convert the JSON string to an object if request was successful
       if (data.length === 0) {
-        console.log("Cat breed not found.");
+        let breedNotFoundErrorMsg = "Cat breed not found.";
+        callback(breedNotFoundErrorMsg, null);
       } else {
         let breedDescription = data[0]["description"]; // access first entry in data array and print out description
-        console.log(breedDescription);
+        callback(null, breedDescription);
       }
     }
   });
 };
 
-let breedName = commandLine[0];
-fetchBreedDescription(breedName);
+
+module.exports = { fetchBreedDescription };
